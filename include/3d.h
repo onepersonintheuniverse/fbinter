99441f1cfc7984a941e61aedc8756c8af8826077
Kıvanç Tok <tokkivanc13@gmail.com>
2026-03-01 16:06:46 +0300

    descriptive fields for wframe

--- include/3d.h
+++ include/3d.h
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include "points.h"
 struct wframe {
-    int vc, ec;
-    struct point3 *v;
-    int *e1, *e2;
+    int nvertices, nedges;
+    struct point3 *vertices;
+    int *edge_from, *edge_to;
 };
 
 void render_wframe(uint32_t *buf, int (*col)(int, int), struct wframe wf, double xradius) {
-    struct fbpos *f = calloc(wf.vc, sizeof(struct fbpos));
-    for (int i = 0; i < wf.vc; ++i) f[i] = p3tofbuf(wf.v[i], xradius);
-    for (int i = 0; i < wf.ec; ++i) render_line_f(buf, col, f[wf.e1[i]], f[wf.e2[i]]);
+    struct fbpos *f = calloc(wf.nvertices, sizeof(struct fbpos));
+    for (int i = 0; i < wf.nvertices; ++i) f[i] = p3tofbuf(wf.vertices[i], xradius);
+    for (int i = 0; i < wf.nedges; ++i) render_line_f(buf, col, f[wf.edge_from[i]], f[wf.edge_to[i]]);
     free(f);
 }
 
 void rotate_wframe(struct wframe *wf, struct point3 *around, double angle, int ax1, int ax2) {
-    for (int i = 0; i < wf->vc; ++i) rotate(&wf->v[i], around, angle, ax1, ax2);
+    for (int i = 0; i < wf->nvertices; ++i) rotate(&wf->vertices[i], around, angle, ax1, ax2);
 }
 #endif
