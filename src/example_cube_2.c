ab7d6f56fb69bf6877cdf84d7163879b7540715e
Kıvanç Tok <tokkivanc13@gmail.com>
2026-03-03 22:33:35 +0300

    added unorthodox version of example_cube.c

--- src/example_cube_2.c
+++ src/example_cube_2.c
@@ -41,14 +41,13 @@ int main(int argc, char **argv) {
     struct timespec rq = {0, 5000000}, rm;
     while (avg_s < 5) {
         gettimeofday(&t1, NULL);
-        render_func_drm(buf, zero);
         double rx = next()/(double)UINT64_MAX, ry = next()/(double)UINT64_MAX, rz = next()/(double)UINT64_MAX;
         xdir *= rx < thresh*dt ? -1 : 1, ydir *= ry < thresh*dt ? -1 : 1, zdir *= rz < thresh*dt ? -1 : 1;
-        render_wframe(buf, white, wf, 2.0);
-        memcpy(s.map, buf, s.size);
+        render_wframe(s.map, zero, wf, 2.0);
         rotate_wframe(&wf, &cube_center, xdir*spd*dt, 2, 3);
         rotate_wframe(&wf, &cube_center, ydir*spd*dt, 1, 3);
         rotate_wframe(&wf, &cube_center, zdir*spd*dt, 1, 2);
+        render_wframe(s.map, white, wf, 2.0);
         // nanosleep(&rq, &rm);
         gettimeofday(&t2, NULL);
         avg_s += dt = t2.tv_sec-t1.tv_sec+(t2.tv_usec-t1.tv_usec)*1e-6;
